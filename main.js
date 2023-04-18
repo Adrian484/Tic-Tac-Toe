b80dc61ce57995bf20d64ec9754463ab8e8e6916
Adrian <Adrian.Davis484@outlook.com>
2023-04-18 14:48:40 +0930

    changed file names for github

---
+++ main.js
@@ -0,0 +1,16 @@
+let clickCount = 0;
+const cells = document.querySelectorAll('table tr td'); 
+cells.forEach(cell => {
+   cell.addEventListener('click', handleClick) 
+})
+
+function handleClick(event) {
+const cell = event.target;
+if (event.target.textContent === "") {
+    event.target.textContent = 'X'
+}
+    clickCount++;
+if (clickCount % 2 === 1) {
+    cell.textContent = 'X';
+} else {cell.textContent = 'O'};
+} 
